a9f21ca848fd2f7991d8814feec1690c62bec269
Jefferson Pedro <jeffersonpedro300@gmail.com>
2023-08-06 15:23:08 -0300

    adicionado rotas de nevagação da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
-// import { HomePage } from "./components/Pages/HomePage/Home.page";
+import { HomePage } from "./components/Pages/HomePage/Home.page";
 import { ContactPage } from "./components/Pages/ContactPage/Contact.page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <ContactPage />
+      <Router>
+        <Routes>
+          <Route path="\" element={<HomePage />} />
+          <Route path="\products" element={<></>} />
+          <Route path="\faq" element={<ContactPage />} />
+          <Route path="\error" element={<></>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
